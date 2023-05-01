a3e2f774a8c628bb2a3a16c76fe0575d4f60d49d
darynamm <darynamanuilenko@gmail.com>
2023-05-01 08:41:47 -0600

    imported a cute picture of a panda and trying to make it be in the. project

--- src/api/view/ActivityFrame.java
+++ src/api/view/ActivityFrame.java
@@ -1,19 +1,39 @@
 package api.view;
 
 import javax.swing.JFrame;
-
+import javax.swing.ImageIcon;
 import api.controller.Controller;
 import api.view.Activity;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import java.net.URL;
 
 
 public class ActivityFrame extends JFrame
 {
 
 	public static void main(String[] args) {
+		
+	
+		ImageIcon logo = new ImageIcon("");
+		
+		
 		JFrame frame = new JFrame();
-		frame.setTitle("Activities");
+		frame.setTitle("Activities from the Bored API :)");
 		frame.setSize(800,600);
 		
+		frame.setLocationRelativeTo(null);
+		frame.setResizable(false);
+		
+		ImageIcon panda = new ImageIcon("boredpanda.png");
+		
+
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		frame.setIconImage(panda.getImage());
 		frame.setVisible(true);
+		
+		
 	}
 }
