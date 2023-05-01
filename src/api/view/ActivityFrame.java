eb3a8096ff26ca1e70f57fbc7c4ddb4def8cfc8e
darynamm <darynamanuilenko@gmail.com>
2023-05-01 07:41:50 -0600

    made some changes so that the code has less erros (imported jframe and changed the name on some imports

--- src/api/view/ActivityFrame.java
+++ src/api/view/ActivityFrame.java
@@ -1,7 +1,9 @@
 package api.view;
 
-import controller.ActivityController;
-import model.Activity;
+import javax.swing.JFrame;
+
+import api.controller.Controller;
+import api.view.Activity;
 
 
 public class ActivityFrame extends JFrame
