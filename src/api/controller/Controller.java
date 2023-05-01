f9db072a6c70f6e301a5a30e155ac9a81e36e255
darynamm <darynamanuilenko@gmail.com>
2023-05-01 08:14:15 -0600

    imported the code from turtle jar

--- src/api/controller/Controller.java
+++ src/api/controller/Controller.java
@@ -3,7 +3,10 @@ package api.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import api.view.Activity;
+import media.model.Turtle;
+import media.model.World;
 
+import java.awt.Color;
 import java.io.IOException;
 import java.net.URL;
 
@@ -12,7 +15,40 @@ public class Controller
 	
 	public void start()
 	{
+		Activity myFirstWorld = new Activity(false);
+		Activity myTurtle = new Activity(myFirstWorld);
+		Activity newTurtle = new Activity(myFirstWorld);
+		
+		myTurtle.forward(30);
+		newTurtle.forward(15);
+		
+		Activity otherTurtle = new Activity(myFirstWorld);
+		otherTurtle.setColor(Color.magenta);
+		
+		Activity othernewTurtle = new Activity(myFirstWorld);
+		othernewTurtle.setColor(Color.blue);
+		
+		for (int index = 0; index< 10; index++)
+		{
+			myTurtle.turn(45);;
+		    myTurtle.forward(100);
+		    
 
+		    
+		    int randomTurn = (int) (Math.random() * 90);
+		    int randomDistance = (int) (Math.random() * 91) + 10; 
+		    
+		    otherTurtle.turn(45);
+		    otherTurtle.backward(randomDistance);
+		    othernewTurtle.turn(randomTurn);
+		    othernewTurtle.backward(20);
+		    othernewTurtle.setPenWidth(1*(index + 1));
+		    
+		   
+		}
+		
+		myFirstWorld.setVisible(true);
+	}
 
 	}
 	private static final String API_URL = "https://www.boredapi.com/api/activity/";
