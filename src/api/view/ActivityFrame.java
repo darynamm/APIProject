2703a17f4bc412d837fe6f8a4d9debf732fc9938
darynamm <darynamanuilenko@gmail.com>
2023-05-01 09:12:17 -0600

    made some buttons

--- src/api/view/ActivityFrame.java
+++ src/api/view/ActivityFrame.java
@@ -2,6 +2,8 @@ package api.view;
 
 import javax.swing.JFrame;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
+
 import api.controller.Controller;
 import api.view.Activity;
 import javax.swing.JPanel;
@@ -9,6 +11,7 @@ import javax.swing.JLabel;
 import javax.swing.ImageIcon;
 
 import java.awt.Color;
+import java.awt.FlowLayout;
 import java.net.URL;
 
 public class ActivityFrame extends JFrame
@@ -36,11 +39,20 @@ public class ActivityFrame extends JFrame
 		this.setLocationRelativeTo(null);
 		this.setResizable(false);
 
-		// adding image
 		
 		Color PRETTY_PURPLE = new Color(198, 115, 255);
 		getContentPane().setBackground(new java.awt.Color(198, 115, 255));
 		
+		
+		frame.setLayout(new FlowLayout());
+		
+		
+		JButton name = new JButton("name");
+		JButton type = new JButton("type");
+		JButton participants = new JButton("participants");
+		JButton accessibility = new JButton("accessibility");
+		JButton price = new JButton("price");
+		JButton imageLink = new JButton("imageLink");
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		this.setVisible(true);
