57559c35ef4d97b482f10241915782fe2cffb041
darynamm <darynamanuilenko@gmail.com>
2023-05-11 07:42:37 -0600

    put the code in the panel from the frame and connected the frame back to the panel

--- src/api/view/ActivityPanel.java
+++ src/api/view/ActivityPanel.java
@@ -22,4 +22,69 @@ public class ActivityPanel extends JPanel {
 		add(accessibilityLabel);
 		add(priceLabel);
 	}
+	
+	private void SetupFrame()
+	{
+		ImageIcon panda = new ImageIcon("boredpanda.png");
+		JLabel label = new JLabel();
+		label.setIcon(panda);
+		this.add(label);
+		this.getRootPane().add(label);
+		ImageIcon logo = new ImageIcon("");
+
+		JFrame frame = new JFrame();
+		
+		this.setSize(800, 600);
+
+		this.setLocation(null);
+		this.setVisible(false);
+
+		
+		Color PRETTY_PURPLE = new Color(198, 115, 255);
+		getRootPane().setBackground(new java.awt.Color(198, 115, 255));
+		
+		
+		frame.setLayout(new FlowLayout());
+		
+		
+		JButton name = new JButton("name");
+		name.setSize(15, 15);	
+		
+		JButton type = new JButton("type");
+		type.setSize(15, 15);	
+		
+		JButton participants = new JButton("participants");
+		participants.setSize(15, 15);	
+		
+		JButton accessibility = new JButton("accessibility");
+		accessibility.setSize(15, 15);
+		
+
+		JButton price = new JButton("price");
+		price.setSize(15, 15);
+		
+		
+		JButton imageLink = new JButton("imageLink");
+		imageLink.setSize(15, 15);
+		
+		this.add(name);
+		this.add(type);
+		this.add(participants);
+		this.add(accessibility);
+		this.add(price);
+		this.add(name);
+		this.add(imageLink);
+		
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		this.setVisible(true);
+
+	}
+
+	private void setDefaultCloseOperation(int exitOnClose)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
 }
